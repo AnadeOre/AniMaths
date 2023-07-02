8c90a8b49bea45ca26e4401d4c11f705d4e6b63e
Ana <anaemilia.deorellana@gmail.com>
2023-07-02 20:24:50 -0300

    Add back button to Conics page

--- src/app/components/PostLayout.jsx
+++ src/app/components/PostLayout.jsx
@@ -1,9 +1,9 @@
 import Layout from './Layout';
 import stylesPosts from './Posts.module.css';
 
-export default function PostLayout({children, titleName}) {
+export default function PostLayout({children, titleName, whereTo}) {
   return (
-    <Layout whereTo='/Conics'>
+    <Layout whereTo={whereTo}>
       <br />
       <div className={stylesPosts.content}>
         <h1>{titleName}</h1>
