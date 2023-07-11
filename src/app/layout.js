9783d03b2061060a6b3efe1a4c415c3ed5e2b7a0
Ana <anaemilia.deorellana@gmail.com>
2023-07-11 18:08:55 -0300

    Add translation to main page

--- src/app/layout.js
+++ src/app/layout.js
@@ -3,22 +3,28 @@ import './styles/globals.css';
 import styles from './styles/layout.module.css';
 import ThemeMode from './components/ThemeMode';
 import Footer from './components/Footer';
-// import LangToggle from './components/LangToggle';
+import LangToggle from './components/LangToggle';
+
+import store from './store.js';
+import {Provider} from 'react-redux';
+
 export default function RootLayout({children}) {
   return (
-    <html lang='en' theme='light'>
-      <title>AniMaths</title>
-      <meta name='description' content='Learn maths with animations' />
-      <body>
-        <div className={styles.topRight}>
-          <div>
-            <ThemeMode />
-            {/* <LangToggle /> */}
+    <Provider store={store}>
+      <html lang='en' theme='light'>
+        <title>AniMaths</title>
+        <meta name='description' content='Learn maths with animations' />
+        <body>
+          <div className={styles.topRight}>
+            <div>
+              <ThemeMode />
+              <LangToggle />
+            </div>
           </div>
-        </div>
-        <main>{children}</main>
-        <Footer />
-      </body>
-    </html>
+          <main>{children}</main>
+          <Footer />
+        </body>
+      </html>
+    </Provider>
   );
 }
